5ebb1ddb7c26d97a6af5aa0c95442fe561d79713
Daiwei <daiweilu123@gmail.com>
2016-04-24 13:23:42 -0400

    Simplify config

--- config/default.js
+++ config/default.js
@@ -1,6 +1,43 @@
 'use strict';
 
+const pkg = require('../package.json');
+
 module.exports = {
-  isDev: !process.env.NODE_ENV || process.env.NODE_ENV === 'development',
-  currentEnv: process.env.NODE_ENV || 'development',
+  cookie: {
+    keys: [
+      process.env.COOKIE_KEY1,
+      process.env.COOKIE_KEY2,
+    ],
+  },
+  redis: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASS,
+  },
+  postgres: {
+    host: 'postgres',
+    port: process.env.POSTGRES_PORT_5432_TCP_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASS,
+  },
+  github: {
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.GITHUB_CALLBACK_URL,
+  },
+  mixpanel: {
+    token: null,
+  },
+  logging: {
+    Logentries: {
+      token: process.env.LOG_ENTRIES_TOKEN,
+    },
+    Sentry: {
+      dsn: process.env.SENTRY_DSN,
+      options: {
+        release: pkg.version,
+      }
+    },
+  },
 };
